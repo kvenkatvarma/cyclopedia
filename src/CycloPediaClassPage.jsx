978563808b52efb28079099f392fc75400335d81
Venkat varma <kvenkatvarma@gmail.com>
2025-02-18 12:01:04 +0530

    Display Instructor

---
+++ src/CycloPediaClassPage.jsx
@@ -0,0 +1,57 @@
+import React,{Component} from "react";
+import {getRandomUser} from "./Utility/api";
+
+class CycloPediaClassPage extends React.Component{
+        constructor(props)
+        {
+            super(props);
+            this.state ={
+                instructor: undefined,
+                studentList:[],
+                studentCount:0,
+                hideInstructor : false
+            };
+        }
+componentDidMount=async()=>
+{
+    console.log("Component Did Mount");
+    const response = await getRandomUser();
+    this.setState((prevstate)=>{
+        return{
+            instructor:{
+                name: response.data.first_name + " " + response.data.last_name,
+                email: response.data.email,
+                phone: response.data.phone_number,
+            }
+        }
+    })   
+}
+componentDidUpdate(){
+    console.log("Component Did Update");
+
+}
+componentWillUnmount()
+{
+    console.log("Component Will Unmount");
+
+}
+render(){
+    console.log("render component");
+    return(
+    <div>
+        {this.state.instructor && (
+            <div className="p-3">
+                  <span className="h4 text-success">Instructor &nbsp;</span>
+                  <i className="bi bi-toggle-off btn btn-success btn-sm"></i>
+                  <br/>
+                  Name: {this.state.instructor.name} <br/>
+                  Email: {this.state.instructor.email} <br/>
+                  Phone: {this.state.instructor.phone} <br/>
+
+            </div>
+        )}
+    </div>
+    );
+}
+}
+export default CycloPediaClassPage;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Header';
+import CycloPediaClassPage from './CycloPediaClassPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<Header/>
+  <div>
+  <Header />
+  <div className="row text-white">
+    <div className="col-6">
+      <span className="h1 text-warning text-center">Class Component</span>
+        <CycloPediaClassPage/>
+    </div>
+   
+  </div>
+</div>
+
 );
 
